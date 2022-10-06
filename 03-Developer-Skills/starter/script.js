fc745648f52ad6e1142d621088f949caaca0d31f
gendo-kun <gendokun@protonmail.com>
2022-10-06 16:48:16 +0200

    commit

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -21,3 +21,40 @@ function spinWords(string) {
 
 console.log(spinWords(input));
 */
+// PROBLEM 1:
+// We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
+
+const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+
+// 1) Understanding the problem:
+/* - What is temp amplitude ? Answer Difference between highest and lowest temp
+How to calculate the maximum and minimum temperaturs ?
+What does a sensor error look like ? What to do when one occurs ? (Ignore error data)
+
+
+// 2) Breaking up into sub-problems:
+// How to Ignore Sensor Errors ?
+- find  max value in temp array
+- find min values in temp arry
+- subtract min from max and return (amplitude)
+*/
+
+const calcTempAmplitude = function (arr, arr2) {
+  //Loop for string and remove it.
+  const ArrayConcat = arr.concat(arr2);
+  for (let i = 0; i < ArrayConcat.length; i++) {
+    if (typeof ArrayConcat[i] !== "number") ArrayConcat.splice(i, 1);
+  }
+  return Math.max(...ArrayConcat) - Math.min(...ArrayConcat);
+};
+
+console.log(calcTempAmplitude(temperatures, [3, 5, 7, -2, 3, -12]));
+
+// PROBLEM 2:
+// Function should now receive 2 arrays of temps
+
+// 1) Understanding the problem
+// - With 2 arrays, should we implement functionality twice? NO! Just merge two arrays
+
+// 2) Breaking up into sub-problems
+// - Merge 2 arrays
