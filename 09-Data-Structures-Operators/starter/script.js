890f93947cde147aea1338c227e91c5281b9ffd1
gendo-kun <gendokun@protonmail.com>
2022-10-19 17:18:48 +0200

    .

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -11,6 +11,9 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 
   openingHours: {
     thu: {
@@ -26,4 +29,54 @@ const restaurant = {
       close: 24,
     },
   },
+  orderDelivery: function (obj) {
+    console.log(obj);
+  },
 };
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Straße 2',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(menu);
+// const { name, openingHours, categories } = restaurant;
+
+// console.log(name, openingHours, categories);
+
+// const {
+//   name: restaurantName,
+//   openingHours: hours,
+//   categories: tags,
+//   menu: menu = [],
+// } = restaurant;
+// console.log(restaurantName, openingHours, tags);
+
+// let a = 111;
+// let b = 999;
+// const obj = { a: 23, b: 7, c: 14 };
+// ({ a, b } = obj);
+
+// const {
+//   fri: { open, close },
+// } = openingHours;
+
+// const arr = [2, 3, 4];
+
+// const [x, y, z] = arr;
+// console.log(x, y, z);
+
+// let [main, , secondary] = restaurant.categories;
+// console.log(main, secondary);
+
+// [main, secondary] = [secondary, main];
+// [secondary, main] = restaurant.order(2, 0);
+// console.log(main, secondary);
+
+// const nested = [2, 4, [5, 6]];
+
+// const [i, , [j, k]] = nested;
+// console.log(i, j, k);
--- 18-forkify/final/src/js/config.js
+++ 18-forkify/final/src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '7bd396e5-dd74-4685-b90c-8b311c489415';
 export const MODAL_CLOSE_SEC = 2.5;
