052a2ec89135e5d99d442c744ad44e703776427a
gendokun <gendokun@protonmail.de>
2022-10-19 17:26:01 +0200

    .

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+function calcAge(bithYear) {
+  const age = 2037 - bithYear;
+
+  function printAge() {
+    const output = `You are ${age}, born in ${bithYear}`;
+    console.log(output);
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Peter';
+calcAge(1991);
